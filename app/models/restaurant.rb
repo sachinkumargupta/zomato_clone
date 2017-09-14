ac3a575da49edd50ee84df5d57d57e2cb55bda2d
sachinkumargupta <sachin7870020331@gmail.com>
2017-09-15 00:03:48 +0530

    styled index page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
 
-  has_attached_file :cover_photo, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :cover_photo, styles: { large: "600x600>", medium: "300x300#", thumb: "100x100#" }
   validates_attachment_content_type :cover_photo, content_type: /\Aimage\/.*\z/
 
   has_many :reviews, dependent: :destroy
